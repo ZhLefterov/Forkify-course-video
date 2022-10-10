f0c370200a2db6c4ce6fd97b7fd408d13d70b903
ZhLefterov <m1nd_as7ray@yahoo.com>
2022-10-10 15:08:58 +0300

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = `https://forkify-api.herokuapp.com/api/v2/recipes/`;
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'aafbf349-782a-410c-8277-548cb013440d';
+export const MODAL_CLOSE_SEC = 2.5;
